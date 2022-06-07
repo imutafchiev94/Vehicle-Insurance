a12690e82fbfb8a83e858e9c468f8b2e3d578b28
Ivaylo Mutafchiev <stu1901322006@uni-plovdiv.bg>
2022-06-07 09:31:26 +0300

    Add Model Insurer

---
+++ Server/models/Insurer.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+let InsurerSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        minlength: 5,
+        maxlength: 15,
+        unique: true
+    }, passwordHash: {
+        type: String,
+        required: true
+    }, email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v)
+            }, message: props => `${props.value} is not valid email!`
+        }
+    }, firstName: {
+        type: String,
+        required: true,
+    }, lastName: {
+        type: String,
+        required: true
+    }
+})
