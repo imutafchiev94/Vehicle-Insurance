043bf7b811242f540233cda78147ee35b792a1c3
Ivaylo Mutafchiev <stu1901322006@uni-plovdiv.bg>
2022-06-07 11:22:28 +0300

    Add routes
    Add routes for controllers

--- Server/routes.js
+++ Server/routes.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 const ownerController = require('./controllers/ownerController');
+const vehicleController = require('./controllers/vehicleController');
+const insuranceController = require('./controllers/insuranceController');
+const paymentController = require('./controllers/paymentController');
 
-
-router.use('/owner', ownerController)
-
-//TODO: Add login and register and roles for insurance agent and for the owner of vehicle
+router.use('/owner', ownerController);
+router.use('/vehicle', vehicleController);
+router.use('/insurance', insuranceController);
+router.use('/payment', paymentController);
+router.get('*', (req, res) => {
+    res.status(404).json({message: 'Page not found!'});
+})
 
 module.exports = router;
