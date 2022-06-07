9d9bc5813b25782fd6017f458240c03c4f1d4290
Ivaylo Mutafchiev <stu1901322006@uni-plovdiv.bg>
2022-06-07 13:26:13 +0300

    Add more logic in vehicle service
    Add logic for finding all vehicles of owner by owner's EGN and logic for finding vehicle by registration number

--- Server/services/vehicleService.js
+++ Server/services/vehicleService.js
@@ -1,21 +1,50 @@
-const Vehicle = require('../models/Vehicle');
+const Vehicle = require("../models/Vehicle");
+const Owner = require("../models/Owner");
 
 async function addVehicle(data) {
-    let newVehicleData = {
-        brand: data.brand,
-        model: data.model,
-        yearOfManufacture: data.yearOfManufacture,
-        registrationNumber: data.registrationNumber
+  let newVehicleData = {
+    brand: data.brand,
+    model: data.model,
+    yearOfManufacture: data.yearOfManufacture,
+    registrationNumber: data.registrationNumber,
+  };
+
+  let newVehicle = new Vehicle(newVehicleData);
+
+  await newVehicle.save();
+
+  return { message: "Vehicle was add successfully!" };
+}
+
+async function findAllVehiclesOfOwner(EGN) {
+  let owner = await Owner.findOne({
+    EGN: EGN,
+  })
+  .populate('vehicles');
+
+  if (!owner) {
+    throw { message: `Owner with EGN "${EGN}" doesn't exist in our database` };
+  }
+
+  let vehicles = owner.vehicles;
+
+  return vehicles;
+};
+
+async function findVehicle(registrationNumber) {
+    let vehicle = await Vehicle.findOne({
+        registrationNumber: registrationNumber
+    });
+
+    if(!vehicle) {
+        throw { message: `Vehicle with this registration number "${registrationNumber}" doesn't exist in our database` };
     }
 
-    let newVehicle = new Vehicle(newVehicleData);
-
-    await newVehicle.save();
-
-    return {message: 'Vehicle was add successfully!'};
+    return vehicle;
 }
-
 
 module.exports = {
-    addVehicle
-}
+  addVehicle,
+  findAllVehiclesOfOwner,
+  findVehicle
+};
