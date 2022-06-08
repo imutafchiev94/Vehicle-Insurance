910a00568802fad47afa3fb8f28ae0add8166c38
Ivaylo Mutafchiev <69365693+imutafchiev94@users.noreply.github.com>
2022-06-08 10:53:54 +0300

    Add Routes

--- Client/vehicle-insurance/src/app/app-routing.module.ts
+++ Client/vehicle-insurance/src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllPaidPaymentsComponent } from './payment/all-paid-payments/all-paid-payments.component';
+import { AllPaymentsComponent } from './payment/all-payments/all-payments.component';
+import { AllUnpaidPaymentsComponent } from './payment/all-unpaid-payments/all-unpaid-payments.component';
+import { HomeComponent } from './home/home.component';
+import { InsuranceComponent } from './insurance/insurance.component';
+import { OwnerComponent } from './owner/owner.component';
+import { PaymentToPayComponent } from './payment/payment-to-pay/payment-to-pay.component';
+import { PaymentComponent } from './payment/payment.component';
+import { VehicleComponent } from './vehicle/vehicle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'owners', component: OwnerComponent},
+  {path: 'vehicles', component: VehicleComponent},
+  {path: 'payments', component: PaymentComponent,
+    children: [
+      {path: '', redirectTo: 'all', pathMatch: 'full'},
+      {path: 'all', component: AllPaymentsComponent},
+      {path: 'all-paid', component: AllPaidPaymentsComponent},
+      {path: 'all-unpaid', component: AllUnpaidPaymentsComponent},
+      {path: 'to-pay', component: PaymentToPayComponent}
+    ]
+  },
+  {path: 'insurances', component: InsuranceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
