0cdddba336020ab21a18e4c0b39fb5150727e360
Ivaylo Mutafchiev <69365693+imutafchiev94@users.noreply.github.com>
2022-06-16 13:15:10 +0300

    remove insurer model in server and remove middlewares

--- Server/config/expressConfig.js
+++ Server/config/expressConfig.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const auth = require('../middlewares/auth')
 
 function setupExpress(app) {
     app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
-    app.use(cookieParser());
-    app.use(auth());
 }
 
 module.exports = setupExpress;
--- Server/middlewares/isInsurer.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-    if(!req.user) {
-        res.status(401).end();
-    }
-    next();
-}
